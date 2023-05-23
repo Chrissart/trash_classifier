0c2810223d59e691d390bb2b78897ed7cae2293b
Christian Urbina <urbinagomezc@gmail.com>
2023-05-23 11:43:17 -0600

    No predice bien de la forma en que estoy pasando la imagen a la función de predicción

--- main.py
+++ main.py
@@ -1,12 +1,73 @@
+"""
+
+"""
 import streamlit
 from streamlit_webrtc import webrtc_streamer
+from keras.models import load_model
+from keras.utils import plot_model
+import numpy
+import cv2
 
-streamlit.title("Trash Classifier")
+from src.predict_image import predict_image
 
+# Configuración de la página
+streamlit.set_page_config(
+    page_title="Trash Classifier",
+    page_icon="🗑️",
+)
+# # Carga el modelo desde el archivo
+model = load_model("./model/ResNet101V2_TrashClassifierV1.h5")
 
-picture = streamlit.camera_input("Take a picture")
+# Contenido de la página
+streamlit.title("Trash Classifier 🗑️")
+# # Agrega un selector de vistas en la barra lateral
+view = streamlit.sidebar.selectbox(
+        label="Selecciona una vista",
+        options=[
+            "Modo Foto",
+            "Modo Streaming",
+            "Acerca de"
+        ]
+    )
+# # Dependiendo de la opción seleccionada, muestra una vista diferente
+if view == "Modo Foto":
+    picture = streamlit.camera_input("Captura un residuo")
+    photo = streamlit.sidebar.file_uploader(
+                "Arrastra y suelta una imagen aquí",
+                type=[
+                    'png',
+                    'jpg',
+                    'jpeg',
+                ]
+            )
+    # # Verifica si se subió una imagen
+    if picture or photo:
+        if picture:
+            streamlit.image(picture, caption="Imagen capturada")
+            image_array = numpy.asarray(bytearray(picture.read()), dtype=numpy.uint8)
+            image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+            image = cv2.resize(image, (150, 150))
 
-if picture:
-    streamlit.image(picture)
-    
-webrtc_streamer(key="example")
+        else:
+            streamlit.image(photo, caption="Imagen cargada")
+            image = numpy.asarray(photo)
+
+        proceso = streamlit.sidebar.button("Procesar!")
+        proceso = streamlit.button("Procesar!", use_container_width=True)
+
+        if proceso:
+            streamlit.write("Procesando...")
+            # Aquí deberías agregar el código para procesar la imagen con tu modelo predictivo
+            predicted_class, confidence = predict_image(model, image)
+            if predicted_class:
+                streamlit.write(f"El residuo es: {predicted_class} con un {confidence*100:.2f}% de confianza.")
+    else:
+        streamlit.write("Por favor, sube una imagen.")
+elif view == "Modo Streaming":
+    # # Código para la segunda vista
+    streamlit.write("Bienvenido a la segunda vista.")
+    webrtc_streamer(key="example", sendback_audio=False)
+elif view == "Acerca de":
+    # # Código para la tercera vista
+    streamlit.write(model)
+    # streamlit.write(plot_model(model))
--- src/predict_image.py
+++ src/predict_image.py
@@ -5,12 +5,28 @@ import numpy as np
 # classes = {'cardboard': 0, 'glass': 1, 'metal': 2, 'paper': 3, 'plastic': 4, 'trash': 5}
 classes = {0: 'cardboard', 1: 'glass', 2: 'metal', 3: 'paper', 4: 'plastic', 5: 'trash'}
 
-def predict_image(model, image, img_size=(150, 150)):
-    # Load the image
-    img = load_img(image.file, target_size=img_size)
+# def predict_image(model, image, img_size=(150, 150)):
+#     print(image)
+#     # Load the image
+#     img = load_img(image, target_size=img_size)
     
+#     # Convert the image to a numpy array and scale the pixel values to the range [0,1]
+#     img_array = img_to_array(img) / 255.0
+
+#     # Add an extra dimension to the array, because the model expects batches of images
+#     img_batch = np.expand_dims(img_array, axis=0)
+
+#     # Get the model's predictions for this batch of images
+#     predictions = model.predict(img_batch)
+
+#     # Get the index of the highest score in the predictions array
+#     predicted_class = np.argmax(predictions[0])
+    
+#     return classes[predicted_class], max(predictions[0])
+
+def predict_image(model, image, img_size=(150, 150)):
     # Convert the image to a numpy array and scale the pixel values to the range [0,1]
-    img_array = img_to_array(img) / 255.0
+    img_array = image.astype(np.float32) / 255.0
 
     # Add an extra dimension to the array, because the model expects batches of images
     img_batch = np.expand_dims(img_array, axis=0)
@@ -20,5 +36,6 @@ def predict_image(model, image, img_size=(150, 150)):
 
     # Get the index of the highest score in the predictions array
     predicted_class = np.argmax(predictions[0])
+    
+    return classes[predicted_class], max(predictions[0])
 
-    return classes[predicted_class]
